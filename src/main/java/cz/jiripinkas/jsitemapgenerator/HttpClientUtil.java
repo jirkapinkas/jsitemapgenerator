88f78e41a2423923dfce9da6003e2fca120bba54
jirka.pinkas@gmail.com <2DUYcOnWoHeLtjskqVBotRN2k>
2018-12-23 15:28:03 +0100

    removed httpclient in favour of okhttp

--- src/main/java/cz/jiripinkas/jsitemapgenerator/HttpClientUtil.java
+++ src/main/java/cz/jiripinkas/jsitemapgenerator/HttpClientUtil.java
@@ -1,51 +1,27 @@
 package cz.jiripinkas.jsitemapgenerator;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class HttpClientUtil {
 
-	/**
-	 * HTTP GET to URL, return status
-	 * 
-	 * @param url
-	 *            URL
-	 * @return status code (for example 200)
-	 * @throws Exception When error
-	 */
-	public static int get(String url) throws Exception {
-
-		// The underlying HTTP connection is still held by the response object
-		// to allow the response content to be streamed directly from the
-		// network socket.
-		// In order to ensure correct deallocation of system resources
-		// the user MUST either fully consume the response content or abort
-		// request
-		// execution by calling HttpGet#releaseConnection().
-
-		int resultCode = 0;
-		DefaultHttpClient httpclient = null;
-		HttpGet httpGet = null;
-		try {
-			httpclient = new DefaultHttpClient();
-			httpGet = new HttpGet(url);
-
-			HttpResponse response = httpclient.execute(httpGet);
-
-			resultCode = response.getStatusLine().getStatusCode();
-			HttpEntity entity = response.getEntity();
-			// do something useful with the response body
-			// and ensure it is fully consumed
-			EntityUtils.consume(entity);
-
-		} catch (Exception ex) {
-			throw new Exception("error sending HTTP GET to this URL: " + url);
-		} finally {
-			httpGet.releaseConnection();
-		}
-		return resultCode;
-	}
+    /**
+     * HTTP GET to URL, return status
+     *
+     * @param url URL
+     * @return status code (for example 200)
+     * @throws Exception When error
+     */
+    public static int get(String url) throws Exception {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        Response response = client.newCall(request).execute();
+        if (!response.isSuccessful()) {
+            throw new Exception("error sending HTTP GET to this URL: " + url);
+        }
+        return response.code();
+    }
 }
