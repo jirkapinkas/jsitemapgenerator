af02cd70f1dc5eb62610ceeb3f1cdec17d984422
jirka.pinkas@gmail.com <2DUYcOnWoHeLtjskqVBotRN2k>
2018-12-23 16:30:04 +0100

    fixed https://github.com/jirkapinkas/jsitemapgenerator/issues/7 Please use URL as the second sort criteria after priority to ensure the sitemap always looks the same regardless of how the pages/images are ordered when they are added to the sitemap.

--- src/main/java/cz/jiripinkas/jsitemapgenerator/WebPage.java
+++ src/main/java/cz/jiripinkas/jsitemapgenerator/WebPage.java
@@ -2,102 +2,126 @@ package cz.jiripinkas.jsitemapgenerator;
 
 import cz.jiripinkas.jsitemapgenerator.exception.InvalidPriorityException;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 public class WebPage implements Comparable<WebPage> {
-	private String name;
-	private Date lastMod;
-	private ChangeFreq changeFreq;
-	private Double priority;
-	private String shortDescription;
-	private String shortName;
+    private String name;
+    private Date lastMod;
+    private ChangeFreq changeFreq;
+    private Double priority;
+    private String shortDescription;
+    private String shortName;
 
-	private List<Image> images;
+    private List<Image> images;
 
-	public void addImage(Image image) {
-		if (images == null) {
-			images = new ArrayList<>();
-		}
-		images.add(image);
-	}
+    public void addImage(Image image) {
+        if (images == null) {
+            images = new ArrayList<>();
+        }
+        images.add(image);
+    }
 
-	public List<Image> getImages() {
-		return images;
-	}
+    public List<Image> getImages() {
+        return images;
+    }
 
-	public void setImages(List<Image> images) {
-		this.images = images;
-	}
+    public void setImages(List<Image> images) {
+        this.images = images;
+    }
 
-	public String getShortName() {
-		return shortName;
-	}
+    public String getShortName() {
+        return shortName;
+    }
 
-	public WebPage setShortName(String shortName) {
-		this.shortName = shortName;
-		return this;
-	}
+    public WebPage setShortName(String shortName) {
+        this.shortName = shortName;
+        return this;
+    }
 
-	public String getShortDescription() {
-		return shortDescription;
-	}
+    public String getShortDescription() {
+        return shortDescription;
+    }
 
-	public WebPage setShortDescription(String shortDescription) {
-		this.shortDescription = shortDescription;
-		return this;
-	}
+    public WebPage setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+        return this;
+    }
 
-	public WebPage setName(String name) {
-		this.name = name;
-		return this;
-	}
+    public WebPage setName(String name) {
+        this.name = name;
+        return this;
+    }
 
-	public WebPage setLastMod(Date lastMod) {
-		this.lastMod = lastMod;
-		return this;
-	}
+    public WebPage setLastMod(Date lastMod) {
+        this.lastMod = lastMod;
+        return this;
+    }
 
-	public WebPage setChangeFreq(ChangeFreq changeFreq) {
-		this.changeFreq = changeFreq;
-		return this;
-	}
+    public WebPage setChangeFreq(ChangeFreq changeFreq) {
+        this.changeFreq = changeFreq;
+        return this;
+    }
 
-	public WebPage setPriority(Double priority) {
-		if (priority < 0.0 || priority > 1.0) {
-			throw new InvalidPriorityException("Priority must be between 0 and 1.0");
-		}
-		this.priority = priority;
-		return this;
-	}
+    public WebPage setPriority(Double priority) {
+        if (priority < 0.0 || priority > 1.0) {
+            throw new InvalidPriorityException("Priority must be between 0 and 1.0");
+        }
+        this.priority = priority;
+        return this;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public Date getLastMod() {
-		return lastMod;
-	}
+    public Date getLastMod() {
+        return lastMod;
+    }
 
-	public ChangeFreq getChangeFreq() {
-		return changeFreq;
-	}
+    public ChangeFreq getChangeFreq() {
+        return changeFreq;
+    }
 
-	public Double getPriority() {
-		return priority;
-	}
+    public Double getPriority() {
+        return priority;
+    }
 
-	public int compareTo(WebPage o) {
-		if (this.getPriority() == null && o.getPriority() == null) {
-			return 0;
-		}
-		if (this.getPriority() == null) {
-			return 1;
-		}
-		if (o.getPriority() == null) {
-			return -1;
-		}
-		return -this.getPriority().compareTo(o.getPriority());
-	}
+    private Comparator<Double> priorityComparator = (a, b) -> {
+        if (a == null && b == null) {
+            return 0;
+        } else if (a == null) {
+            return 1;
+        } else if (b == null) {
+            return -1;
+        }
+        return -Double.compare(a, b);
+    };
+
+    private Comparator<String> shortNameComparator = (a, b) -> {
+        if (a == null && b == null) {
+            return 0;
+        } else if (a == null) {
+            return 1;
+        } else if (b == null) {
+            return -1;
+        }
+        return a.compareTo(b);
+    };
+
+    /**
+     * Compare WebPage first by priority (in descending order - higher priority is first), then by shortName (in ascending order).
+     * Priority and / or shortName can be null. WebPages with null priority are at the end.
+     * @param o
+     * @return
+     */
+    public int compareTo(WebPage o) {
+        int result;
+        // first compare by priority
+        result = priorityComparator.compare(this.getPriority(), o.getPriority());
+        // next compare by shortName
+        if (result == 0) {
+            result = shortNameComparator.compare(this.getShortName(), o.getShortName());
+        }
+        return result;
+    }
 }
--- src/test/java/cz/jiripinkas/jsitemapgenerator/generator/SitemapPathTest.java
+++ src/test/java/cz/jiripinkas/jsitemapgenerator/generator/SitemapPathTest.java
@@ -1,37 +1,39 @@
 package cz.jiripinkas.jsitemapgenerator.generator;
 
-import java.text.DateFormat;
-import java.util.Date;
-
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
 import cz.jiripinkas.jsitemapgenerator.ChangeFreq;
 import cz.jiripinkas.jsitemapgenerator.W3CDateFormat;
 import cz.jiripinkas.jsitemapgenerator.WebPage;
-import cz.jiripinkas.jsitemapgenerator.generator.SitemapGenerator;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+import static org.junit.Assert.*;
 
 public class SitemapPathTest {
 
-    private SitemapGenerator sitemapGenerator;
-    private DateFormat dateFormat = new W3CDateFormat();
-    private Date lastModDate;
+    private DateFormat dateFormat;
 
     @Before
-    public void setUp() throws Exception {
-        sitemapGenerator = new SitemapGenerator("http://www.javavids.com/");
-        lastModDate = new Date();
-        sitemapGenerator.addPage(new WebPage().setName("/index.php").setPriority(1.0).setChangeFreq(ChangeFreq.NEVER)
-                .setLastMod(lastModDate));
-        sitemapGenerator.addPage(new WebPage().setName("basepath/latest.php"));
-        sitemapGenerator.addPage(new WebPage().setName("/basepath/contact.php"));
+    public void setUp() {
+        dateFormat = new W3CDateFormat();
     }
 
     @Test
     public void testSitemapPaths() {
+        SitemapGenerator sitemapGenerator = new SitemapGenerator("http://www.javavids.com/");
+        Date lastModDate = new Date();
+        sitemapGenerator.addPage(new WebPage()
+                .setName("/index.php")
+                .setPriority(1.0)
+                .setChangeFreq(ChangeFreq.NEVER)
+                .setLastMod(lastModDate));
+        sitemapGenerator.addPage(new WebPage().setName("basepath/latest.php"));
+        sitemapGenerator.addPage(new WebPage().setName("/basepath/contact.php"));
+
         String sitemap = sitemapGenerator.constructSitemapString();
-        String expected = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+        final String expected = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
                 + "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n"
                 + "<url>\n"
                 + "<loc>http://www.javavids.com/index.php</loc>\n"
@@ -45,7 +47,54 @@ public class SitemapPathTest {
                 + "<loc>http://www.javavids.com/basepath/latest.php</loc>\n"
                 + "</url>\n"
                 + "</urlset>";
-        Assert.assertEquals(sitemap, expected);
+        assertEquals(sitemap, expected);
+    }
+
+    @Test
+    public void testSitemapPaths2() {
+        SitemapGenerator sitemapGenerator = new SitemapGenerator("http://www.javavids.com/");
+        sitemapGenerator.addPage(new WebPage()
+                .setName("/x0")
+                .setPriority(1.0));
+        sitemapGenerator.addPage(new WebPage()
+                .setName("/a0")
+                .setPriority(1.0));
+        sitemapGenerator.addPage(new WebPage()
+                .setName("/x1")
+                .setPriority(0.5));
+        sitemapGenerator.addPage(new WebPage()
+                .setName("/a1")
+                .setPriority(0.5));
+        sitemapGenerator.addPage(new WebPage().setName("x2"));
+        sitemapGenerator.addPage(new WebPage().setName("a2"));
+
+        String sitemap = sitemapGenerator.constructSitemapString();
+        final String expected = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n" +
+                "<url>\n" +
+                "<loc>http://www.javavids.com/a0</loc>\n" +
+                "<priority>1.0</priority>\n" +
+                "</url>\n" +
+                "<url>\n" +
+                "<loc>http://www.javavids.com/x0</loc>\n" +
+                "<priority>1.0</priority>\n" +
+                "</url>\n" +
+                "<url>\n" +
+                "<loc>http://www.javavids.com/a1</loc>\n" +
+                "<priority>0.5</priority>\n" +
+                "</url>\n" +
+                "<url>\n" +
+                "<loc>http://www.javavids.com/x1</loc>\n" +
+                "<priority>0.5</priority>\n" +
+                "</url>\n" +
+                "<url>\n" +
+                "<loc>http://www.javavids.com/a2</loc>\n" +
+                "</url>\n" +
+                "<url>\n" +
+                "<loc>http://www.javavids.com/x2</loc>\n" +
+                "</url>\n" +
+                "</urlset>";
+        assertEquals(sitemap, expected);
     }
 
 }
